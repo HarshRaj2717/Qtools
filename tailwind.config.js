16f9f095a7b38d423279550c409ad3dc738df913
harshraj2717 <harshraj2717@gmail.com>
2023-05-30 22:23:32 +0530

    Add base files for daisyUI and autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   content: ["./*", "./src/**/*.{html,js}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '760px',
-      lg: '976px',
-      xl: '1440px'
+      sm: "480px",
+      md: "760px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("daisyui"),
+  ],
 };
