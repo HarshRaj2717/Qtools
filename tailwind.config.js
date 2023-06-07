87ba13d8ca056d3e1f7c7acc49d85e65185beb11
harshraj2717 <harshraj2717@gmail.com>
2023-06-08 02:39:53 +0530

    create pricing section + under development page

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
   ],
   daisyui: {
     styled: true,
-    themes: ["night", "winter", "luxury"],
+    themes: ["night", "winter"],
     base: true,
     utils: true,
     logs: true,
