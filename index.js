a0140a9739213fd613ac7a5b5894439fb15f1971
harshraj2717 <harshraj2717@gmail.com>
2023-06-20 02:00:36 +0530

    organise all code using ejs-layouts + add a tools router + minor frontend changes + improvements for faster & easier development

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/general");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { pageTitle: "" });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { pageTitle: "Login" });
 });
 
 app.get("/api", (req, res) => {
-  res.render("api");
+  res.render("api", { pageTitle: "API" });
 });
+
+app.get("/tools", (req, res) => {
+  res.redirect("/#tools");
+});
+
+const toolsRouter = require("./routes/tools");
+app.use("/tools", toolsRouter);
 
 // 404 page
 app.use((req, res, next) => {
   res.status(404);
-  res.render("404");
+  res.render("404", { pageTitle: "404" });
 });
 
 app.listen(PORT, () => {
